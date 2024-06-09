2f5b84422d64167843317251c009bed52d1a4b8a
precious <jkpresh@gmail.com>
2024-06-09 11:58:06 +0000

    modified cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -75,7 +75,7 @@ def put_city(city_id):
         abort(404)
     data = request.get_json()
     if not data:
-        abort(400, 'Not a JSON')
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
     for key, value in data.items():
         if key not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, key, value)
